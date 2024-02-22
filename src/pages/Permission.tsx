6e42cc951dae483738e483e9cf845845e3575197
Faishal Pasa <faishal.pasa@bareksa.com>
2024-02-22 18:22:01 +0700

    feat: setting menu

--- src/pages/Permission.tsx
+++ src/pages/Permission.tsx
@@ -279,7 +279,7 @@ function PagePermission() {
       </div>
 
       <Modal open={modalForm.open} title={modalForm.title}>
-        <form autoComplete="off" className="grid grid-cols-2 gap-4 p-6">
+        <form autoComplete="off" className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-6">
           <Input
             placeholder="Nama Permission"
             label="Nama Permission"

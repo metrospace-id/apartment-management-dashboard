a48efc2e27d3e6bccc676fe1d036152ca9947ed5
Faishal Pasa <faishal.pasa@bareksa.com>
2024-05-06 11:05:00 +0700

    deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -176,7 +176,7 @@ function Home() {
           <div className="rounded-lg bg-blue-600 dark:bg-slate-950 text-white dark:text-white p-4">
             <div className="flex justify-between">
               <p className="text-sm sm:text-md font-semibold">Jumlah Unit</p>
-              <div className="p-4 rounded-full bg-blue-200 hidden sm:block">
+              <div className="p-2 lg:p-4 rounded-full bg-blue-200 hidden md:!block">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-slate-600">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
@@ -189,7 +189,7 @@ function Home() {
           <div className="rounded-lg bg-green-600 dark:bg-slate-950 text-white dark:text-white p-4">
             <div className="flex justify-between">
               <p className="text-sm sm:text-md font-semibold">Jumlah Pemilik</p>
-              <div className="p-4 rounded-full bg-green-200 hidden sm:block">
+              <div className="p-2 lg:p-4 rounded-full bg-green-200 hidden md:!block">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-slate-600">
                   <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
                 </svg>
@@ -202,7 +202,7 @@ function Home() {
           <div className="rounded-lg bg-yellow-600 dark:bg-slate-950 text-white dark:text-white p-4">
             <div className="flex justify-between">
               <p className="text-sm sm:text-md font-semibold">Jumlah Penghuni</p>
-              <div className="p-4 rounded-full bg-yellow-200 hidden sm:block">
+              <div className="p-2 lg:p-4 rounded-full bg-yellow-200 hidden md:!block">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-slate-600">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
@@ -215,7 +215,7 @@ function Home() {
           <div className="rounded-lg bg-purple-600 dark:bg-slate-950 text-white dark:text-white p-4">
             <div className="flex justify-between">
               <p className="text-sm sm:text-md font-semibold">Jumlah Kendaraan</p>
-              <div className="p-4 rounded-full bg-purple-200 hidden sm:block">
+              <div className="p-2 lg:p-4 rounded-full bg-purple-200 hidden md:!block">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-slate-600">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
                 </svg>
